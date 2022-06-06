33ac83e0675b3ff3110ba41c4bc1c4f7c024468e
Erik Somnäs <erik.somnas@gmail.com>
2022-06-06 14:03:28 +0200

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+const posts = [
+    { title: 'React Testing', excerpt: 'Learn React Testing' },
+    { title: 'React With Tailwind', excerpt: 'Learn React with Tailwind' }
+];
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <Head>
+                <title>CMS Blog</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className='lg:col-span-8 col-span-1'>
+                    {posts.map((post, index) => (
+                        <div>
+                            {post.title}
+                            {post.excerpt}
+                        </div>
+                    ))}
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
