e28dbe65acb1603a46ea33028152de2211d23a67
Erik Somnäs <erik.somnas@gmail.com>
2022-06-06 14:15:54 +0200

    export components

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function PostCard() {
-  return (
-    <div>PostCard</div>
-  )
+    return (
+        <div>PostCard</div>
+    )
 }
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function PostWidget() {
-  return (
-    <div>PostWidget</div>
-  )
+    return (
+        <div>PostWidget</div>
+    )
 }
