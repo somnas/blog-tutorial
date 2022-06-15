d766985d9b13a6884628749361b492035cd6697d
Erik Somnäs <erik.somnas@gmail.com>
2022-06-15 20:16:30 +0200

    header wip

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 
 import Link from 'next/link';
 
-const categories = [{name: 'React', slug: 'react'}, {name: 'Web Development', slug: 'web-dev'}]
+const categories = [{ name: 'React', slug: 'react' }, { name: 'Web Development', slug: 'web-dev' }]
 
 export default function Header() {
     return (
@@ -17,7 +17,11 @@ export default function Header() {
                 </div>
                 <div className='hidden md:float-left md:contents'>
                     {categories.map(category => (
-                        <Link key={category.slug} href={`/category/${category.slug}`}>{category.name}</Link>
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                                {category.name}
+                            </span>
+                        </Link>
                     ))}
                 </div>
             </div>
