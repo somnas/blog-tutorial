d3c1ac747474e4d2d3eb6b542868536efc1658d1
Erik Somnäs <erik.somnas@gmail.com>
2022-06-15 20:13:50 +0200

    create Header component

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+
+import Link from 'next/link';
+
+const categories = [{name: 'React', slug: 'react'}, {name: 'Web Development', slug: 'web-dev'}]
+
+export default function Header() {
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='border-b w-full inline-block border-blue-400 py-8'>
+                <div className='md:float-left block'>
+                    <Link href='/'>
+                        <span className='cursor-pointer font-bold text-4xl text-white'>
+                            GraphCMS
+                        </span>
+                    </Link>
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map(category => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>{category.name}</Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
